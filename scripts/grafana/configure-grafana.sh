15f05a6ee9cf86b5fad33eec07d0851a7c849dc0
Robertas Simkevicius <robertas.simkevicius@layerv.com>
2019-03-16 09:12:06 +0200

    Fixed az-cli package and added dashboards

--- scripts/grafana/configure-grafana.sh
+++ scripts/grafana/configure-grafana.sh
@@ -92,11 +92,15 @@ function post_json() {
 }
 
 #install azure-cli
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
-     sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
-sudo apt-get install apt-transport-https
-sudo apt-get update && sudo apt-get install azure-cli
+sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+     --keyserver packages.microsoft.com \
+     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+sudo apt-get update -y
+sudo apt-get install azure-cli -y
 
 #get vitrual machines
 az login --service-principal -u $CLIENT_ID --password $CLIENT_SECRET --tenant $TENANT_ID
