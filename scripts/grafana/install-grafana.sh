6914f4860d78d543b83ec57a4054891dbc0435c7
Robertas Simkevicius <robertas.simkevicius@layerv.com>
2019-03-16 11:44:26 +0200

    force apt-get install

--- scripts/grafana/install-grafana.sh
+++ scripts/grafana/install-grafana.sh
@@ -58,7 +58,7 @@ install_grafana()
     wget "${DOWNLOAD_URL}"
     sudo dpkg -i "grafana_${GRAFANA_VERSION}_amd64.deb"
     systemctl daemon-reload
-    sudo apt-get install -y
+    sudo apt-get -f install -y
 }
 
 start_grafana()
